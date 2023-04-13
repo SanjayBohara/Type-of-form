f8f427be43752958d879b8bdf1aabf63d30b643a
Sanjay Bohara <sanjayboharasbbb@gmail.com>
2023-04-13 23:19:10 +0530

    practice JS

---
+++ js/prectics.js
@@ -0,0 +1,44 @@
+// function cheackDriverAge () {
+// var age = prompt("what is your age?")
+
+// if(Number(age)<18) {
+//     alert("sorry you can/'t drive");
+// }
+// else if(Number(age)>18) {
+//     alert("drive safe ,start the engine");
+// }
+// else if(Number(age)===18) {
+//     alert("your fist driving congratulation");
+// }
+// }
+
+
+// var checkDriverAge2 = function() {
+// 	var age = prompt("What is your age?");
+// 	if (Number(age) < 18) {
+// 		alert("Sorry, you are too yound to drive this car. Powering off");
+// 	} else if (Number(age) > 18) {
+// 		alert("Powering On. Enjoy the ride!");
+// 	} else if (Number(age) === 18) {
+// 		alert("Congratulations on your first year of driving. Enjoy the ride!");
+// 	}
+// }
+
+
+// array //
+
+// array.shift()
+// var array = ["banana","orange","apple","nali"]
+// array.push("kivi");
+
+
+
+
+// ***Dom Events
+// chaking click working or not
+
+// var button = document.getElementsByTagName("button")[0]
+
+// button.addEventListener("click", function() {
+//     console.log("click!!!")
+// })
